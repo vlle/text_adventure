9a125f97b0b03a49991a84613a447ab983553905
artemiikulikov <aapqwhat@gmail.com>
2023-07-07 22:33:35 +0300

    :hatching_chick: init commit

---
+++ restful_db/internal/database/create_tables.sql
@@ -0,0 +1,66 @@
+-- postgresql dialect
+-- # # #
+-- # # #
+-- # # #
+
+CREATE TABLE IF NOT EXISTS image (
+  id         serial primary key,
+  name       varchar(50) not null,
+  url        varchar(50),
+  emoji      varchar(50) not null,
+  CONSTRAINT unique_emoji UNIQUE (emoji)
+);
+
+CREATE TABLE IF NOT EXISTS p_user (
+  id         serial primary key,
+  name       varchar(50) not null,
+  password   varchar(50) not null,
+  image_id   integer references image(id)
+);
+
+
+INSERT INTO image (name, emoji) VALUES
+('Bug', '🐛'),
+('Dragon', '🐉'),
+('Ghost', '👻'),
+('Alien', '👽'),
+('Robot', '🤖'),
+('Monster', '👾'),
+('Key', '🔑'),
+('Door', '🚪'),
+('Bed', '🛏️'),
+('Detective', '🕵️‍♂️');
+
+
+CREATE TABLE IF NOT EXISTS location (
+  id          serial primary key,
+  title       varchar(50) not null,
+  image_id    integer references image(id),
+  description varchar(50) not null,
+  xy          point,
+  constraint unique_title unique (title)
+);
+
+CREATE TABLE IF NOT EXISTS item (
+  id         serial primary key,
+  name       varchar(50) not null,
+  description varchar(50) not null,
+  location_id integer references location(id),
+  image_id    integer references image(id),
+  CONSTRAINT unique_name UNIQUE (name)
+);
+
+INSERT INTO item (name, description) VALUES
+('Key', 'Old rusty key, it may open something important'),
+('Hammer', 'Be careful, you might hit someone with it'),
+('Lantern', 'You really should not turn light off');
+
+CREATE TABLE IF NOT EXISTS monster (
+  id         serial primary key,
+  name       varchar(50) not null,
+  description varchar(50) not null,
+  location_id integer references location(id),
+  image_id    integer references image(id),
+  CONSTRAINT unique_name UNIQUE (name)
+);
+
---
+++ restful_db/internal/database/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+  "github.com/jackc/pgx/v5/pgxpool"
+)
+
+func main() {
+  urlExample := "postgres://postgres:postgres@localhost:5500/rec"
+  dbpool, err := pgxpool.New(context.Background(), urlExample)
+  if err != nil {
+    fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+    os.Exit(1)
+  }
+  defer dbpool.Close()
+
+  var title string
+  var artist string
+  _, err = dbpool.Exec(context.Background(), "insert into album (title, artist, price) values ('The Dark Side of the Moon', 'Pink Floyd', 100)")
+  if err != nil {
+    fmt.Fprintf(os.Stderr, "Exec failed: %v\n", err)
+    os.Exit(1)
+  }
+  err = dbpool.QueryRow(context.Background(), "select title, artist from album where id = $1", 3).Scan(&title, &artist)
+  if err != nil {
+    fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+    os.Exit(1)
+  }
+
+  fmt.Println(title, artist)
+}
