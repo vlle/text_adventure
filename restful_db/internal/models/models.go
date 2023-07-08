34475577b8ea31e3aa5971e08a95e6cb1ec6cb6c
artemiikulikov <aapqwhat@gmail.com>
2023-07-09 00:45:15 +0300

    :hammer: update: add tags to models structs

--- restful_db/internal/models/models.go
+++ restful_db/internal/models/models.go
@@ -1,45 +1,45 @@
 package models
 
 type Image struct {
-  ID int
-  Name string
-  Url string
-  Emoji string
+  ID int `json:"id"`
+  Name string `json:"name"`
+  Url string `json:"url"`
+  Emoji string `json:"emoji"`
 }
 
 type Point struct {
-  X int
-  Y int
+  X int `json:"x"`
+  Y int `json:"y"`
 }
 
 type Location struct {
-  ID int
-  Title string
-  XY Point
-  Description string
-  ImageID int
+  ID int `json:"id"`
+  Title string `json:"title"`
+  XY Point `json:"xy"`
+  Description string `json:"description"`
+  ImageID int `json:"image_id"`
 }
 
 type User struct {
-  ID int
-  Name string
+  ID int `json:"id"`
+  Name string `json:"name"`
 
-  ImageID int
-  LocationID int
-}
+  ImageID int `json:"image_id"`
+  LocationID int `json:"location_id"`
+} 
 
 type Item struct {
-  ID          int
-  Title       string
-  Description string
-  LocationID int
-  ImageID    int
+  ID          int `json:"id"`
+  Title       string `json:"title"`
+  Description string `json:"description"`
+  LocationID int `json:"location_id"`
+  ImageID    int `json:"image_id"`
 }
 
 type Monster struct {
-  ID int
-  Title string
-  Description string
-  LocationID int
-  ImageID int
+  ID int `json:"id"`
+  Title string `json:"title"`
+  Description string `json:"description"`
+  LocationID int `json:"location_id"`
+  ImageID int `json:"image_id"`
 }
